534501a7d6ce670cbccdfeab20165eee7914504f
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2022-07-08 05:28:29 +0000

    Slightly improve the Run program

--- Sources/Run/app.swift
+++ Sources/Run/app.swift
@@ -6,9 +6,9 @@ struct App {
     static func main() async throws {
         Backtrace.install()
         let disovery = try await FlyAppDiscovery(port: 8080)
-        print("Starting discovery...")
+        print("Subscribing to service discovery.")
         for try await first in disovery.subscribe(to: .currentApp()) {
-            print("In loop:", first)
+            print(first)
         }
     }
 }
